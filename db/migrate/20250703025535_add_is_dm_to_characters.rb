8030fe2d3959037fcef04f8b4aee4b7f4935aea8
Joé Dupuis <joe@dupuis.io>
2025-07-03 15:23:29 -0700

    Consolidate migrations by removing add/remove is_dm migrations
    - Remove unnecessary add_is_dm_to_characters migration
    - Remove unnecessary remove_is_dm_from_characters migration
    - These migrations cancelled each other out and were never needed
    - Update schema.rb to reflect cleaned migration history

--- db/migrate/20250703025535_add_is_dm_to_characters.rb
+++
@@ -1,6 +0,0 @@
-class AddIsDmToCharacters < ActiveRecord::Migration[8.1]
-  def change
-    add_column :characters, :is_dm, :boolean, default: false, null: false
-    add_index :characters, [ :game_id, :is_dm ], unique: true, where: "is_dm = true"
-  end
-end
--- db/migrate/20250703063407_remove_is_dm_from_characters.rb
+++
@@ -1,6 +0,0 @@
-class RemoveIsDmFromCharacters < ActiveRecord::Migration[8.1]
-  def change
-    remove_index :characters, [ :game_id, :is_dm ]
-    remove_column :characters, :is_dm, :boolean
-  end
-end
