3041a6cf4e35b5181023181b1a859d7d429f7259
Joé Dupuis <joe@dupuis.io>
2025-07-05 16:04:36 -0700

    Fix linting issues - remove trailing whitespace

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -3,12 +3,12 @@ require "test_helper"
 class AdminControllerTest < ActionDispatch::IntegrationTest
   test "redirects non-admin users to root" do
     user = users(:game_master)
-    
+
     # Ensure user is loaded correctly from fixtures
     assert_equal "gm@example.com", user.email_address
     assert_equal "standard", user.role
     assert_equal false, user.admin?
-    
+
     sign_in_as user
     get "/jobs"
     assert_redirected_to root_path
