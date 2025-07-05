641ed01bde962ee54b052c1e8e12a474afe52246
Joé Dupuis <joe@dupuis.io>
2025-07-05 16:00:49 -0700

    Fix Mission Control configuration timing issue
    Remove after_initialize wrapper - it was causing the base_controller_class
    to not be set properly in test environment, allowing non-admins to access
    Mission Control. Now configuration happens immediately when initializer loads.

--- config/initializers/mission_control_jobs.rb
+++ config/initializers/mission_control_jobs.rb
@@ -1,4 +1,3 @@
-Rails.application.config.after_initialize do
-  MissionControl::Jobs.base_controller_class = "AdminController"
-  MissionControl::Jobs.http_basic_auth_enabled = false
-end
+# Configure Mission Control Jobs
+MissionControl::Jobs.base_controller_class = "AdminController"
+MissionControl::Jobs.http_basic_auth_enabled = false
--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -2,7 +2,14 @@ require "test_helper"
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
   test "redirects non-admin users to root" do
-    sign_in_as users(:game_master)
+    user = users(:game_master)
+    
+    # Ensure user is loaded correctly from fixtures
+    assert_equal "gm@example.com", user.email_address
+    assert_equal "standard", user.role
+    assert_equal false, user.admin?
+    
+    sign_in_as user
     get "/jobs"
     assert_redirected_to root_path
   end
@@ -14,11 +21,7 @@ class AdminControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "redirects unauthenticated users to sign in" do
-    skip "Mission Control Jobs server_id parameter causes routing issues"
     get "/jobs"
-    # Check we get redirected (the exact path might vary due to engine routing)
-    assert_response :redirect
-    follow_redirect!
-    assert_match(/session\/new/, path)
+    assert_redirected_to new_session_path
   end
 end
